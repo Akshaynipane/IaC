514cf36f42179b023193fc323f4803f12e16acdd
Akshaynipane <47139163+Akshaynipane@users.noreply.github.com>
2025-08-22 14:26:14 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,6 +1,22 @@
-# Input variable: user names
-variable "user_names" {
-  description = "Create IAM users with these names"
-  type        = "list"
-  default     = ["neo", "trinity", "morpheus"]
-}
+AWSTemplateFormatVersion: '2010-09-09'
+Description: S3 bucket with public read ACL
+
+Resources:
+  PublicReadS3Bucket:
+    Type: AWS::S3::Bucket
+    Properties:
+      BucketName: your-unique-public-read-bucket-name # Replace with a globally unique bucket name
+      AccessControl: PublicRead
+      OwnershipControls:
+        Rules:
+          - ObjectOwnership: BucketOwnerPreferred # Or ObjectWriter, depending on your needs
+      PublicAccessBlockConfiguration:
+        BlockPublicAcls: false
+        BlockPublicPolicy: false
+        IgnorePublicAcls: false
+        RestrictPublicBuckets: false
+
+Outputs:
+  BucketName:
+    Description: Name of the S3 bucket
+    Value: !Ref PublicReadS3Bucket
